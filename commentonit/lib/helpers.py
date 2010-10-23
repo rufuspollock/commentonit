62d906aad30b31bfeabd8825e97c68e91caeba2e
rgrp <http://rufuspollock.org@localhost>
2010-10-23 21:30:19 +0100

    [all][l]: remove all existing files (mainly pylons app) except README.txt.

--- commentonit/lib/helpers.py
+++
@@ -1,9 +0,0 @@
-"""Helper functions
-
-Consists of functions to typically be used within templates, but also
-available to Controllers. This module is available to templates as 'h'.
-"""
-from webhelpers.html.tags import *
-from routes import url_for
-from genshi import HTML
-
