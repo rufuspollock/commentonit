ea30df276ccba4ffeb6762c980b1865d2d5322e7
rgrp <http://rufuspollock.org@localhost>
2010-10-25 02:47:02 +0100

    [create,edit][l]: creation/update form for new documents.
      * Basic show/edit.js with usage of mustache (based on sofa)
      * Working and will create documents (though far from perfect)
      * Decent site layout template using mustache partials based on wordpress twentyten template

---
+++ shows/edit.js
@@ -0,0 +1,31 @@
+function(doc, req) {  
+  var ddoc = this;
+  var Mustache = require("vendor/couchapp/lib/mustache");
+  var path = require("vendor/couchapp/lib/path").init(req);
+
+  var indexPath = path.show('edit');
+
+  var data = {
+    header : {
+      index : indexPath,
+      blogName : ddoc.couchapp.name,
+    },
+    scripts : {},
+    pageTitle : doc ? "Edit: "+doc.title : "Create new document",
+    assets : path.asset()
+  };
+  
+  if (doc) {
+    data.doc = JSON.stringify(doc);
+    data.title = doc.title;
+    data.body = doc.body;
+    data.tags = doc.tags.join(", ");
+  } else {
+    data.doc = JSON.stringify({
+      type : "post",
+      format : "markdown"
+    });
+  }
+
+  return Mustache.to_html(ddoc.templates.edit, data, ddoc.templates.partials);
+}
